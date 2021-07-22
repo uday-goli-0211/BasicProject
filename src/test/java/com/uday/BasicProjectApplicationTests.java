b3a7f8da00ca14edb3ea4ea8aaac8d9c7403a2d2
uday goli <rohan.maheshbabu@gmail.com>
2021-07-22 13:04:18 +0530

    added tests

--- src/test/java/com/uday/BasicProjectApplicationTests.java
+++ src/test/java/com/uday/BasicProjectApplicationTests.java
@@ -1,13 +1,32 @@
 package com.uday;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class BasicProjectApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void test() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/test")
+		.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andReturn();
 	}
 
 }
